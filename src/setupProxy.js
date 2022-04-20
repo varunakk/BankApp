43c5a8bd8bb8f4810d223e7f158849c530737ad4
varunakk <56592896+varunakk@users.noreply.github.com>
2022-04-20 22:08:04 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const  createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware("/loan/*", { target: "http://localhost:5000" }));
-  app.use(createProxyMiddleware("localhost:5000/users/*", { target: "http://localhost:5000/users/" }));
+  app.use(createProxyMiddleware("users/*", { target: "http://localhost:5000/users/" }));
   app.use(createProxyMiddleware("/req/*", { target: "http://localhost:5000" })); 
   app.use(createProxyMiddleware("/transactions/*", { target: "http://localhost:5000" })); 
  
