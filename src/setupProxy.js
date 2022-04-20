25097db2ec300e000f71d1763411baf9e7c31dac
varunakk <56592896+varunakk@users.noreply.github.com>
2022-04-20 21:52:21 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,5 +5,5 @@ module.exports = function(app) {
   app.use(createProxyMiddleware("localhost:5000/users/*", { target: "http://localhost:5000" }));
   app.use(createProxyMiddleware("/req/*", { target: "http://localhost:5000" })); 
   app.use(createProxyMiddleware("/transactions/*", { target: "http://localhost:5000" })); 
-  
+ 
 };
