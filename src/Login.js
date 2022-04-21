f207720d5f14796c0e54d8efde4a691103ea59f2
varunakk <56592896+varunakk@users.noreply.github.com>
2022-04-21 14:38:56 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -13,17 +13,37 @@ function Login(){
     
     const LoginUser=()=>{
         let b=1;
-        axios.post("/Login",{username:usrname1,password:Password1 }).then((res)=>{// axios sends the http async req to end points , here we are sending to port o 4000.
-            console.log("log success");
-            store.dispatch({type:"loginSuccess" ,payload:{ user:usrname1} });
+       if(!usrname1) {
+      response.status(400).json("enter username");
+// stop further execution in this callback
+      return;
+  }
+  if(!Password1) {
+      response.status(400).json("enter password");
+// stop further execution in this callback
+      return;
+  }
+//console.log("sds")
+const st="https://iiitsbankbackend.herokuapp.com/users/"+user1;
+console.log(st)
+  axios.get(st).then((res)=>{// axios sends the http async req to end points , here we are sending to port o 4000.
+   console.log("res data  ... ",res.data);
+   if(res.data.password==Password1){
+    console.log("success");
+    response.status(200).json("user authenticated");
+   store.dispatch({type:"loginSuccess" ,payload:{ user:usrname1} });
             history.push("/Main");// redirecting 
-        }).catch((err)=>{
-            console.log("log fail");
-      
-          //  alert("LOG FAIL");
-            store.dispatch({type:"loginFail"});
-        })
-    }
+   }
+}).catch((err)=>{
+//alert("Log Fail");
+   console.log("log fail");
+   response.status(400).json("user does not exist");
+    store.dispatch({type:"loginFail"}); 
+   return;
+  
+})
+
+     }
     return (
         <div className="form-box">
 
